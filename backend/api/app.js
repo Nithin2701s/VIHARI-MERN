66fae658a65220ff4aefb2fe1e130bd573d9338f
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-04-30 19:04:50 +0530

    as

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors =require('cors');
-const helmet = require('helmet');
 const path=require('path');
 const morgan = require('morgan');
 const rfs = require('rotating-file-stream');
 const app = express()
 app.use(express.json())
 
-// app.use(helmet())
-// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-
-// app.use(cors());
+app.use(cors());
 
 const accessLogStream = rfs.createStream("access.log",{
   interval:'1d',
@@ -19,16 +15,16 @@ const accessLogStream = rfs.createStream("access.log",{
 })
 app.use(morgan('combined',{stream:accessLogStream}))
 // Routing..
-app.use(require('./routes/auth'))
+app.use(require('../routes/auth'))
  
-const agentRoutes=require('./routes/agent');
-const adminRoutes=require('./routes/admin');
-const tourRoutes = require('./routes/tour');
-const testRoutes = require('./routes/test')
+const agentRoutes=require('../routes/agent');
+const adminRoutes=require('../routes/admin');
+const tourRoutes = require('../routes/tour');
+const testRoutes = require('../routes/test');
 app.use('/api/agent',agentRoutes);
 app.use('/api/admin',adminRoutes);
 app.use('/api/tour',tourRoutes);
-app.use(require('./routes/bus'))
+app.use(require('../routes/bus'))
 app.use("/api",testRoutes)
 
 
@@ -38,11 +34,16 @@ app.get('/',(req,res)=>{
 
 // inbuilt middleware
 // app.use('/uploads',express.static(__dirname+'/uploads'));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/images',express.static(__dirname+'/public/Images'));
 
 app.use('/error',(req,res,next)=>{
+  if(Error){
   throw new Error("new error")
+  }
+  else {
+    next();
+  }
 })
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -56,7 +57,7 @@ mongoose
     "mongodb+srv://Srikar:Sailu3002@cluster0.ch9hacp.mongodb.net/Vihari"
   ) 
   .then((result) => {
-    app.listen(5000, () => {
+    app.listen(4000, () => {
       console.log("listening to port 4000");
     });
     console.log("Mongodb connected");
