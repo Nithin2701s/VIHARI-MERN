73780e5f6ce7be3c0238c32bc00853ffe3f004e7
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-04-30 19:14:30 +0530

    dxfg

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors =require('cors');
-const path=require('path');
-const morgan = require('morgan');
-const rfs = require('rotating-file-stream');
+// const path=require('path');
+// const morgan = require('morgan');
+// const rfs = require('rotating-file-stream');
 const app = express()
 app.use(express.json())
 
 app.use(cors());
 
-const accessLogStream = rfs.createStream("access.log",{
-  interval:'1d',
-  path:path.join(__dirname, 'logs')
-})
-app.use(morgan('combined',{stream:accessLogStream}))
-// Routing..
-app.use(require('../routes/auth'))
+// const accessLogStream = rfs.createStream("access.log",{
+//   interval:'1d',
+//   path:path.join(__dirname, 'logs')
+// })
+// app.use(morgan('combined',{stream:accessLogStream}))
+// // Routing..
+// app.use(require('../routes/auth'))
  
-const agentRoutes=require('../routes/agent');
-const adminRoutes=require('../routes/admin');
-const tourRoutes = require('../routes/tour');
-const testRoutes = require('../routes/test');
-app.use('/api/agent',agentRoutes);
-app.use('/api/admin',adminRoutes);
-app.use('/api/tour',tourRoutes);
-app.use(require('../routes/bus'))
-app.use("/api",testRoutes)
+// const agentRoutes=require('../routes/agent');
+// const adminRoutes=require('../routes/admin');
+// const tourRoutes = require('../routes/tour');
+// const testRoutes = require('../routes/test');
+// app.use('/api/agent',agentRoutes);
+// app.use('/api/admin',adminRoutes);
+// app.use('/api/tour',tourRoutes);
+// app.use(require('../routes/bus'))
+// app.use("/api",testRoutes)
 
 
 app.get('/',(req,res)=>{
@@ -33,24 +33,9 @@ app.get('/',(req,res)=>{
 })
 
 // inbuilt middleware
-// app.use('/uploads',express.static(__dirname+'/uploads'));
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-app.use('/images',express.static(__dirname+'/public/Images'));
-
-app.use('/error',(req,res,next)=>{
-  if(Error){
-  throw new Error("new error")
-  }
-  else {
-    next();
-  }
-})
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
+// // app.use('/uploads',express.static(__dirname+'/uploads'));
+// app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+// app.use('/images',express.static(__dirname+'/public/Images'));
 
 mongoose
   .connect(
