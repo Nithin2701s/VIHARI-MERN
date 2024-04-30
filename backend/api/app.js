f537bab83ba226851b960ab2c624b5e2cc39dad8
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2024-04-30 19:17:50 +0530

    xc

--- backend/api/app.js
+++ backend/api/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 // const morgan = require('morgan');
 // const rfs = require('rotating-file-stream');
 const app = express()
-app.use(express.json())
+// app.use(express.json())
 
 app.use(cors());
 
